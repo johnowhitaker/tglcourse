310cf976d1299f34b031f38d01f4f34a5a64b7f1
Jonathan Whitaker <johnowhitaker@gmail.com>
2022-10-13 05:59:33 +0200

    Adding script example

---
+++ scripts/example_script.py
@@ -0,0 +1,6 @@
+from fastcore.script import *
+@call_parse
+def main(msg:str,     # The message
+         upper:bool): # Convert to uppercase?
+    "Print `msg`, optionally converting to uppercase"
+    print(msg.upper() if upper else msg)
